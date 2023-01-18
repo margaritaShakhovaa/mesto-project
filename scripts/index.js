09f48fadfcaedadf124ab7aa0399c485f3dd364a
Margarita <alis-weasley@mail.ru>
2023-01-18 19:12:09 +0300

    feat: добавить валидацию формам (профиль и добавление карточки)

--- scripts/index.js
+++ scripts/index.js
@@ -25,6 +25,7 @@ const linkInput = document.querySelector('[name="cardUrl"]');
 const elementPopup = document.querySelector('.element-popup');
 const imagePopupElement = elementPopup.querySelector('.element-popup__image');
 
+
 // ФУНКЦИИ
 // Функция открытия попап форм
 function openPopup(popup) {
@@ -159,3 +160,80 @@ popupEditElement.addEventListener('keydown', (e) => {
       closePopup(popupEditElement);
     }
   })
+
+
+
+// const form = document.querySelector('.form');
+// const formInput = form.querySelector('.popup__input');
+// const formError = form.querySelector(`.${formInput.id}-error`);
+
+// Функция, которая добавляет класс с ошибкой
+const showError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_type_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('popup__input-error_active');
+};
+
+// Функция, которая удаляет класс с ошибкой
+const hideError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_type_error');
+  errorElement.classList.remove('popup__input-error_active');
+  errorElement.textContent = '';
+};
+
+// Функция, которая проверяет валидность поля
+const checkInputValidity = (formElement, inputElement) => {
+  if (inputElement.validity.patternMismatch) {
+    inputElement.setCustomValidity(inputElement.dataset.errorMessage);
+  } else {
+    inputElement.setCustomValidity("");
+  }
+
+  if (!inputElement.validity.valid) {
+    showError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideError(formElement, inputElement);
+  }
+};
+
+const hasInvalidInput = (inputList) => {
+  return inputList.some ((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.disabled = true;
+    buttonElement.classList.add('popup__submit_inactive');
+  } else {
+    buttonElement.disabled = false;
+    buttonElement.classList.remove('popup__submit_inactive');
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__submit');
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
